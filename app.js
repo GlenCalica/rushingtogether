6a3a547485317f702163eeea5c29ad5fe68e6bd4
Glen <glencalica@gmail.com>
2021-01-08 23:19:25 -0500

    Bug Fixes
    - Small style fixes in the mobile menu
    - Removed scrolling when mobile menu is open
    - Move mobile menu with scroll y position

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ const hamburgerOpen = document.getElementById('hamburger-open');
 const hamburgerClose = document.getElementById('hamburger-close');
 const hamburgerMenu = document.querySelector('.mobile-menu');
 
+const siteWrapper = document.querySelector('.site-wrapper');
+
+// let menuOpen = false;
+
 hamburgerOpen.addEventListener('click', () => {
+    console.log(window.scrollY);
+
+    hamburgerMenu.style.top = `${window.scrollY}px`
+
     hamburgerMenu.classList.add('toggle');
-    // document.body.classList.add('lock-scroll');
-})
+    document.body.style.overflowY = 'hidden';
+    // menuOpen = true;
+});
 
 hamburgerClose.addEventListener('click', () => {
     hamburgerMenu.classList.remove('toggle');
-    // document.body.classList.remove('lock-scroll');
-})
+    document.body.style.overflowY = 'visible';
+});
+
+// window.addEventListener('click', (e) => {   
+//     if (!hamburgerMenu.contains(e.target) && menuOpen === true){
+//         setTimeout( closeMenu, 1000 );
+        
+
+//     } else{
+        
+//     }
+// });
+
+// function closeMenu() {
+
+//     menuOpen = false;
+// }
